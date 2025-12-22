8d15c09625ebb98004d64374429fc0a5aae9970e
vaibhav <vaibhav9923@gmail.com>
2025-12-22 15:41:47 +0530

    - Add the image PostRequest and adding product to database

--- src/main/java/com/project1/sampleProject/controller/ProductController.java
+++ src/main/java/com/project1/sampleProject/controller/ProductController.java
@@ -6,11 +6,16 @@ import com.project1.sampleProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -41,6 +46,21 @@ public class ProductController {
         }
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile image){
+
+        try{
+            Product pr1 = productService.addProduct(product,image);
+            return new ResponseEntity<>(pr1,HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage() ,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 
 
--- src/main/java/com/project1/sampleProject/model/Product.java
+++ src/main/java/com/project1/sampleProject/model/Product.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,4 +27,9 @@ public class Product {
     private LocalDate date;
     private boolean available;
     private int quantity;
+
+    private String imageName;
+    private String ImageType;
+    @Lob
+    private byte [] imageData;
 }
